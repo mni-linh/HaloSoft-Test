3eb71834046df14b2896f77c70ab84739fc980c0
Tứ Linh <tranthitulinh1305@gmail.com>
2023-08-25 13:50:04 +0700

    fix ui desktop

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// feature back to top button
 const toTop = document.querySelector(".to-top");
 
 window.addEventListener("scroll", () => {
@@ -6,4 +7,22 @@ window.addEventListener("scroll", () => {
   } else {
     toTop.classList.remove("active");
   }
+});
+// feature previos and next button for slider
+let scrollContainer = document.querySelector(".gallery");
+let backBtn = document.getElementById("backBtn");
+let nextBtn = document.getElementById("nextBtn");
+
+scrollContainer.addEventListener("wheel", (evt) => {
+  evt.preventDefault();
+  scrollContainer.scrollLeft += evt.deltaY;
+  scrollContainer.style.scrollBehavior = "auto";
+});
+backBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft -= 900;
+});
+nextBtn.addEventListener("click", () => {
+  scrollContainer.style.scrollBehavior = "smooth";
+  scrollContainer.scrollLeft += 900;
 });
